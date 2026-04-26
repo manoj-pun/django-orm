06cbbcc1375e58c8f9ab92585d34e5d7931cd469
manoj-pun <manojpun840@gmail.com>
2026-04-26 17:34:12 +0545

    django-orm

---
+++ orm_core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Restaurant,Sale,Rating
+
+admin.site.register(Restaurant)
+admin.site.register(Sale)
+admin.site.register(Rating)
---
+++ orm_core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Restaurant(models.Model):
+    class TypeChoices(models.TextChoices):
+        NEPALESE = 'NE', "Nepalese"
+        CHINESE = "CH", "Chinese"
+        ITALIAN = "IT", "Italian"
+        GREEK = "GR", "Greek"
+        OTHER = "OT", "Other"
+
+    name = models.CharField(max_length=100)
+    website = models.URLField(default="")
+    date_opened = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    restaurant_type = models.CharField(max_length=2, choices=TypeChoices.choices)
+
+    def __str__(self):
+        return self.name
+    
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"Rating: {self.rating}"
+    
+class Sale(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
+    income = models.DecimalField(max_digits=8, decimal_places=2)
+    datetime = models.DateTimeField()
